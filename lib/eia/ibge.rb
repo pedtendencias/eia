bea7a29b54f5f086d6e62ea82f07d2e6267e3193
rCamposCruz <rcampos@tendencias.com.br>
2017-02-03 13:43:22 -0200

    Fixes implementation of test_connection

--- lib/eia/ibge.rb
+++ lib/eia/ibge.rb
@@ -1,10 +1,11 @@
 require 'net/http'
 
 class IBGE
+	def initialize()
+	end
+
 	def test_connection()
-		response = Net::HTTP.get('http://api.sidra.ibge.gov.br/values/t/1612/n1/1')
-		
-		puts response
+		response = Net::HTTP.get(URI('http://api.sidra.ibge.gov.br/values/t/1612/n1/1'))
 
 		if response.to_s == '' then
 			return false
--- lib/eia/version.rb
+++ lib/eia/version.rb
@@ -9,6 +9,7 @@ module Eia
 # Version 0.1.1
 # Fixes gemspec refference to net.
 # Adds refference to ibge.rb in lib/eba.rb
+# Fixes implementation of get in test_connection
 
 
 end
