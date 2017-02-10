22fee6e1d733be96d4f095c3168813e699911b53
rCamposCruz <rcampos@tendencias.com.br>
2017-02-10 11:15:34 -0200

    Fix ruby errors that I had mistakenly made on the tests.

--- spec/eia_spec.rb
+++ spec/eia_spec.rb
@@ -4,21 +4,62 @@ RSpec.describe Eia do
   it "has a version number" do
     expect(Eia::VERSION).not_to be nil
   end
-  
-  before :all do
-    @eia = IBGE.new
-  end
 
-  context "created an valid IBGE instance" do
+  context "creates a valid IBGE instance" do
+	  before :all do
+  	  @con = IBGE.new
+	  end
+
     it "can connect to IBGE" do
-      expect(@eia.test_connection).to eq(true)
+      expect(@con.connected?).to eq(true)
     end
 
-		context "is conected to IBGE" do
-			it "can request a valid result" do
-				#faz alguma coisa
-				expect(false).to eq(true)
+		context "is conected to IBGE and requests a series" do
+			before :all do
+				@valid_series = {code: "1612", period:  "last",
+												 variables: "all", territorial_level: "2|all;",
+												 classification: "81|2702"}
+
+				@invalid_series = {code: "1", period: "last", variables: "all", 
+													 territorial_level: "2|all;", classification: "81|2702"}
 			end
-		end
-  end
+
+			context "which is invalid" do
+				before :all do
+					@invalid_request = @con.get_series(@invalid_series[:code],
+																						 @invalid_series[:period],
+																						 @invalid_series[:variables],
+																						 @invalid_series[:territorial_level],
+																						 @invalid_series[:classification])
+				end
+
+				it "has no results" do
+					expect(@invalid_request.length).to eq(0)
+				end
+			end
+		
+			context "which is valid" do
+				before :all do
+					@valid_request = @con.get_series(@valid_series[:code],
+																					 @valid_series[:period],
+																					 @valid_series[:variables],
+																					 @valid_series[:territorial_level],
+																					 @valid_series[:classification])
+					@item = @valid_request[0]
+				end
+
+				it "has one or more items" do
+					expect(@valid_request.length >= 1).to eq(true)
+				end
+
+				it "has no nils" do
+					expect(@valid_request.include? nil).to eq(false)
+				end
+
+				it "has valid values" do
+					expect(@item.is_valid?).to eq(true)
+				end
+			end
+  	end
+	end
 end
